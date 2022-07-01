8cd19676bac129ddc48d262f7135757e3f861cb9
mlange-42 <martin_lange_@gmx.net>
2022-07-01 17:38:53 +0200

    initial commit

---
+++ src/cdda-tools/__init__.py
@@ -0,0 +1,11 @@
+"""
+Cataclysm DDA Python tools.
+"""
+
+from . import json
+
+try:
+    from ._version import __version__
+except ModuleNotFoundError:  # pragma: nocover
+    # package is not installed
+    __version__ = "0.0.0.dev0"
---
+++ src/cdda-tools/json/__init__.py
@@ -0,0 +1,4 @@
+"""
+JSON file handling.
+"""
+
