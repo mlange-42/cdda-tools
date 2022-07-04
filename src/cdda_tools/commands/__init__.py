470550d847c44cbbec36a4c01c054ae9e7ecbf0a
mlange-42 <martin_lange_@gmx.net>
2022-07-04 14:10:04 +0200

    more import fixes to satisfy isort

--- src/cdda_tools/commands/__init__.py
+++ src/cdda_tools/commands/__init__.py
@@ -1,3 +1,4 @@
+from . import util
 from .command import Command
 from .copy_player import CopyPlayer
 from .copy_vehicle import CopyVehicle
@@ -5,5 +6,3 @@ from .find import Find
 from .note import Note
 from .notes import Notes
 from .vehicle_mod import VehicleMod
-
-from . import notes, util
